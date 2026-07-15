e3927caddb3549eeb5db971145605078ae1cadcf
Bhabderrahim <abderrahimbachaab@gmail.com>
2026-07-15 14:05:36 +0100

    fixe the domain of robots.txt

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -1,9 +1,16 @@
-import { MetadataRoute } from 'next';
-import { SITE_URL } from '@/lib/config';
+// client/src/app/robots.ts
+import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  // Use the env var we set in Vercel
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'https://www.sololife.xyz';
+
   return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: `${SITE_URL}/sitemap.xml`,
-  };
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/sys-admin/', '/api/'],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
 }
--- core/settings.py
+++ core/settings.py
@@ -216,7 +216,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.commonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
